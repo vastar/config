0b6b18cd601826ffe01c14bce54ee42c7a023140
vastar <14828097+vastar@users.noreply.github.com>
2026-06-05 00:05:30 +0300

    yazi config

---
+++ yazi/init.lua
@@ -0,0 +1,10 @@
+function Linemode:size_and_mtime()
+  local time = math.floor(self._file.cha.mtime or 0)
+  if time == 0 then
+    time = ""
+  else
+    time = os.date("%d %b %Y", time)
+  end
+  local size = self._file:size()
+  return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+end
