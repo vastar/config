0b59f2be7f92ac932a43fb79bd7f76e5a7d2a9de
vastar <14828097+vastar@users.noreply.github.com>
2026-06-14 22:16:51 +0300

    nvim: added border to which-key

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -52,7 +52,10 @@ require("textcase").setup({})
 require("which-key").setup({
   delay = function(ctx)
     return ctx.plugin and 0 or 500
-  end
+  end,
+  win = {
+    border = "single"
+  }
 })
 require("trouble").setup({})
 require("snacks").setup({
